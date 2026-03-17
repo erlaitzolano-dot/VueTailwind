463afc64067e2549cdf92d11c2c7a1a1712d1452
Erlaitz Olano <erlaitz.olano@globant.com>
2026-03-17 09:26:03 +0100

    SomosValhalla acabado

--- Valhalla/src/components/SomosValhalla.vue
+++ Valhalla/src/components/SomosValhalla.vue
@@ -5,18 +5,22 @@
                 Somos Valhalla®
             </p>
         </div> 
-        <div class="lg:w-[80%] lg:h-[126px] mx-auto">
+        <div class="lg:w-[80%] lg:h-[126px] mx-auto lg:mb-12">
             <p class="text-[42px] text-white font-bold leading-[42px] tracking-[-1px] lg:mb-16">
                 En VALHALLA® desarrollamos modelos de innovación  abierta para las organizaciones, 
                 conectando el talento interno con el ecosistema emprendedor. 
             </p>
         </div>
-        <div class="lg:w-[80%] lg:h-[126px] mx-auto">
-            <p class="text-[42px] text-white font-bold leading-[42px] tracking-[-1px]">
+        <div class="lg:w-[80%] lg:h-[108px] mx-auto lg:mb-14">
+            <p class="text-[26px] text-white leading-[36px] tracking-[-0.01px]">
                 Ayudamos a las compañías a identificar oportunidades de crecimiento y cambio, y las desarrollamos
                 creando programas de innovación abierta en los que formamos personas, activamos procesos y
                 desarrolamos programas a medida de cada organización. 
             </p>
-        </div>    
+        </div>
+        <div class="lg:w-[80%] lg:h-[22px] mx-auto">
+            <a href="#" class="text-white text-[14px] leading-[24px] tracking-[1px]">CONOCE NUESTRA VISIÓN</a>
+            <img src="../assets/Button/Secondary 1/Dark/Shape Copy.png" alt="Arrow Derecha" class="inline-block ml-2">
+        </div>   
     </div>
 </template>
