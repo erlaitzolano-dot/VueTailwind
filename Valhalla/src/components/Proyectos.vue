66e5e237780c41a8af9a53fee0c280d6f43b9834
Erlaitz Olano <erlaitz.olano@globant.com>
2026-03-17 13:52:18 +0100

    Proyectos acabado

--- Valhalla/src/components/Proyectos.vue
+++ Valhalla/src/components/Proyectos.vue
@@ -10,9 +10,32 @@
                 Hemos innovado con...
             </p>
         </div>
-        <div>
-            <img src="" alt="">
-            <p></p>
+       
+        <div class="hidden lg:block lg:items-center lg:justify-center lg:mx-auto">
+            <div class="lg:w-[1110px] lg:h-[589px] mx-auto relative mb-4">
+                <img src="../assets/Estrella.png" alt="Estrella Galicia" class="object-cover w-full h-full">
+                <div class="absolute flex flex-col gap-2 bottom-10 left-10">
+                    <p class="text-white text-[42px] leading-[42px] tracking-[-1px]">
+                        TheHop<br> by Estrella Galicia
+                    </p>
+                    <p class="uppercase text-white text-[14px] leading-[24px] tracking-[1px]">
+                        Emprendimiento colaborativo
+                    </p>
+                </div>
+            </div>
+            <div class="lg:w-[1110px] lg:h-[540px] mx-auto grid grid-cols-2 gap-4">
+                <div>
+                <img src="../assets/modulo.png" alt="Hermanos Martinez" class="object-cover w-full h-full">
+                </div>
+                <div>
+                    <img src="../assets/lacasa.png" alt="Lacasa" class="object-cover w-full h-full">
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-end lg:mt-16 lg:mr-40">
+            <a href="#" class="text-black text-[14px] tracking-[1px] leading-[24px]">VER TODOS LOS PROYECTOS
+                <span class="text-[10px] ml-1 items-center justify-center">►</span>
+            </a>
         </div>
     </div>
 </template>
